c2eafb35aeeef3c1c422122c7e0c4f0403ec2c17
tom-effernelli <tom.effernelli@gmail.com>
2025-12-15 14:32:30 +0100

    feat: improving Tinput

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 # input is an np.array object
 def Tinput(input):
     Tinput = Image.new('RGB', (len(input[0]), len(input))) # Creating new Image that will have gaussian histogram
+    sortedInputValues = []
+    for y in range(len(input)):
+        for x in range(len(input[0])):
+            sortedInputValues.append((x, y, input[y][x]))
 
 # Texture file must be named after 'texture.jpg' and must thus be a JPEG format
 input_texture = Image.open("texture.jpg").convert('RGB')
