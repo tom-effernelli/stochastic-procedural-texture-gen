c8e0689ee5b550a1bf1d2a9fb0ceecc6ab493f67
tom-effernelli <tom.effernelli@gmail.com>
2025-12-19 14:00:15 +0100

    fix: fixing performance issues

--- app.py
+++ app.py
@@ -82,7 +82,8 @@ def hash(p):
 input_texture = Image.open("texture.jpg").convert('RGB')
 input = np.array(input_texture)
 input_channels = [input[:, :, i] for i in range(3)]
-output_img = Image.new('RGB', (input_texture.size[0]*4, input_texture.size[1]*4))
+t_inputs = [T(ic) for ic in input_channels]
+output_img = Image.new('RGB', (input_texture.size[0]*2, input_texture.size[1]*2))
 output = np.array(output_img)
 output_channels = [output[:, :, i] for i in range(3)]
 
@@ -97,12 +98,12 @@ for c in range(3):
             uv2 = (uv + hash(vertex2))%1
             uv3 = (uv + hash(vertex3))%1
 
-            t_input = T(input_channels[c])
-            G1 = t_input[np.floor(uv1[1]*len(t_input))][np.floor(uv1[0]*len(t_input[0]))]
-            G2 = t_input[np.floor(uv2[1]*len(t_input))][np.floor(uv2[0]*len(t_input[0]))]
-            G3 = t_input[np.floor(uv3[1]*len(t_input))][np.floor(uv3[0]*len(t_input[0]))]
+            
+            G1 = t_inputs[c][np.floor(uv1[1]*(len(t_inputs[c])-1))][np.floor(uv1[0]*(len(t_inputs[c][0])-1))]
+            G2 = t_inputs[c][np.floor(uv2[1]*(len(t_inputs[c])-1))][np.floor(uv2[0]*(len(t_inputs[c][0])-1))]
+            G3 = t_inputs[c][np.floor(uv3[1]*(len(t_inputs[c])-1))][np.floor(uv3[0]*(len(t_inputs[c][0])-1))]
 
             G = w1*G1 + w2*G2 + w3*G3
             G = G - 0.5
-            G = G/(w1**2 + w2**2 + w3**2)
+            G = G/np.sqrt((w1**2 + w2**2 + w3**2))
             G = G + 0.5
